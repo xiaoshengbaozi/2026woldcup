f9d40f4927a711dbd97f12dac8b8bc1518d6aaa1
xiaoshengbaozi <caiyuzhouap@gmail.com>
2026-05-27 20:07:42 +0800

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isPages = process.env.GITHUB_ACTIONS === "true";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: isPages && repo ? `/${repo}` : "",
-  assetPrefix: isPages && repo ? `/${repo}/` : ""
+  }
 };
 
 export default nextConfig;
